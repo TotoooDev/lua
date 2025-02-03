cea47e79c587f1a383401af779752fc3e671e0b9
TotoooDev <totooodev@proton.me>
2025-02-03 15:46:23 +0100

    add files

--- premake5.lua
+++ premake5.lua
@@ -1,13 +1,43 @@
 project "lua"
-    kind "StaticLib"
+    kind "SharedLib"
     language "C"
     targetdir "bin"
 
+    -- based on https://www.lua.org/manual/5.4/readme.html
     files {
-        "**.h",
-        "**.c"
+        "lapi.c",
+        "lcode.c",
+        "lctype.c",
+        "ldebug.c",
+        "ldo.c",
+        "ldump.c",
+        "lfunc.c",
+        "lgc.c",
+        "llex.c",
+        "lmem.c",
+        "lobject.c",
+        "lopcodes.c",
+        "lparser.c",
+        "lstate.c",
+        "lstring.c",
+        "ltable.c",
+        "ltm.c",
+        "lundump.c",
+        "lvm.c",
+        "lzio.c",
+        "lauxlib.c",
+        "lbaselib.c",
+        "lcorolib.c",
+        "ldblib.c",
+        "liolib.c",
+        "lmathlib.c",
+        "loadlib.c",
+        "loslib.c",
+        "lstrlib.c",
+        "ltablib.c",
+        "lutf8lib.c",
+        "linit.c"
     }
 
-    removefiles {
-        "onelua.c"
-    }
+    filter "system:linux"
+        pic "On"
